780d5395342664432b4170c59ed8d18637577750
Alexandre Lunardi <alexandre.lunardi2@gmail.com>
2023-12-02 16:00:24 -0300

    :recycle: refactor form

--- src/components/Form/index.tsx
+++ src/components/Form/index.tsx
@@ -1,71 +1,68 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { v4 as uuidv4 } from "uuid";
 import Button from '../Button';
 import ITask from '../../types/Task';
 import style from './Form.module.scss';
 
-class Form extends React.Component<{
+interface Props {
     setTasks: React.Dispatch<React.SetStateAction<ITask[]>>
-}> {
-    state = {
-        task: "",
-        time: "00:00",
-    }
+}
 
-    addTask(event: React.FormEvent<HTMLFormElement>) {
+function Form({setTasks}: Props) {
+    const [task, setTask] = useState("");
+    const [time, setTime] = useState("00:00");
+    
+    function addTask(event: React.FormEvent<HTMLFormElement>) {
         event.preventDefault();
-        this.props.setTasks(oldTasks => 
+        setTasks(oldTasks => 
             [
                 ...oldTasks,
                 {
-                    ...this.state,
+                    task,
+                    time,
                     selected: false,
                     completed: false,
                     id: uuidv4(),
                 }
             ]
         );
-        this.setState({
-            task: "",
-            time: "00:00:00",
-        })
+        setTask("");
+        setTime("00:00");        
     }
-
-    render() {
-        return (
-            <form className={style.novaTarefa} onSubmit={this.addTask.bind(this)}>
-                <div className={style.inputContainer}>
-                    <label htmlFor='tarefa'>Adicione um nome estudo</label>
-                    <input 
-                        type="text"
-                        name="tarefa"
-                        id="tarefa"
-                        value={this.state.task}
-                        onChange={event => this.setState({...this.state, task: event.target.value})}
-                        placeholder='O que você quer estudar?'
-                        required
-                    />
-                </div>
-                <div className={style.inputContainer}>
-                    <label htmlFor='tempo'>Tempo</label>
-                    <input
-                        type="time"
-                        step='1'
-                        name="tempo"
-                        id="tempo"
-                        value={this.state.time}
-                        onChange={event => this.setState({...this.state, time: event.target.value})}
-                        min="00:00:00"
-                        max="01:30:00"
-                        required                        
-                    />
-                </div>
-                <Button type="submit">
-                    Adicionar
-                </Button>
-            </form>
-        )
-    }
+    
+    return (
+        <form className={style.novaTarefa} onSubmit={addTask}>
+            <div className={style.inputContainer}>
+                <label htmlFor='tarefa'>Adicione um nome estudo</label>
+                <input 
+                    type="text"
+                    name="tarefa"
+                    id="tarefa"
+                    value={task}
+                    onChange={event => setTask(event.target.value)}
+                    placeholder='O que você quer estudar?'
+                    required
+                />
+            </div>
+            <div className={style.inputContainer}>
+                <label htmlFor='tempo'>Tempo</label>
+                <input
+                    type="time"
+                    step='1'
+                    name="tempo"
+                    id="tempo"
+                    value={time}
+                    onChange={event => setTime(event.target.value)}
+                    min="00:00:00"
+                    max="01:30:00"
+                    required                        
+                />
+            </div>
+            <Button type="submit">
+                Adicionar
+            </Button>
+        </form>
+    )    
 }
 
 export default Form;
